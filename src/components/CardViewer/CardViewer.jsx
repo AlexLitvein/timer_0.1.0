8693420e891f0b69a963856d6f3103cf83b6496b
Lexa <amigos.73@mail.ru>
2022-03-12 14:04:02 +0700

    CardViewer

---
+++ src/components/CardViewer/CardViewer.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import CardViewerStyles from './CardViewer.module.css'
+
+export const CardList = ({ list }) => {
+
+    const renderList = () => {
+        return list.map((el) => el)
+    }
+    return (
+        <div className={CardViewerStyles.cardList}>
+            {/* {renderList} */}
+            {list}
+        </div >
+    )
+}
+
+export const CardViewer = (props) => {
+
+
+    return (
+        <div className={CardViewerStyles.cardViewer}>
+            <div className={CardViewerStyles.cardView}>
+dgfdgf
+            </div >
+            <CardList {...props} />
+        </div >
+    )
+}
